8f9bf4be2ada68b37c0136c37f77be1f34c4ced5
Amirhoseinkhd <iamir.kh98@gmail.com>
2024-01-02 15:51:16 +0330

    google font removed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,13 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import '../globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+// import { Roboto, Roboto_Serif } from 'next/font/google'
 import Topbar from '@/Components/shared/Topbar'
 import LeftSidebar from '@/Components/shared/LeftSidebar'
 import RightSidebar from '@/Components/shared/RightSidebar'
 import Bottombar from '@/Components/shared/Bottombar'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Roboto_Serif({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Threads',
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body >
 
           <Topbar />
 
