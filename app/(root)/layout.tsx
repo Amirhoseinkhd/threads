f06a9b77b7c7de72000cf286fe60d3e3f7d72ded
Amirhoseinkhd <iamir.kh98@gmail.com>
2024-01-03 17:54:31 +0330

    discard changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,12 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import '../globals.css'
 import type { Metadata } from 'next'
-// import { Roboto, Roboto_Serif } from 'next/font/google'
+
 import Topbar from '@/Components/shared/Topbar'
 import LeftSidebar from '@/Components/shared/LeftSidebar'
 import RightSidebar from '@/Components/shared/RightSidebar'
 import Bottombar from '@/Components/shared/Bottombar'
 
-// const inter = Roboto_Serif({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Threads',
