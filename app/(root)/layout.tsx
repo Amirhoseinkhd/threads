879ba91c27caa09412143cb5088e5fe50a004089
Amirhoseinkhd <iamir.kh98@gmail.com>
2024-01-17 00:06:48 +0330

    Updated.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,47 +1,49 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import '../globals.css'
-import type { Metadata } from 'next'
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-import Topbar from '@/Components/shared/Topbar'
-import LeftSidebar from '@/Components/shared/LeftSidebar'
-import RightSidebar from '@/Components/shared/RightSidebar'
-import Bottombar from '@/Components/shared/Bottombar'
+import "../globals.css";
+import LeftSidebar from "@/Components/shared/LeftSidebar";
+import Bottombar from "@/Components/shared/Bottombar";
+import RightSidebar from "@/Components/shared/RightSidebar";
+import Topbar from "@/Components/shared/Topbar";
 
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: 'Threads',
-  description: 'A Next.js 13 Meta Threads Application'
-}
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads application",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body >
-
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
+        <body className={inter.className}>
           <Topbar />
 
           <main className='flex flex-row'>
             <LeftSidebar />
-
             <section className='main-container'>
-              <div className='w-full max-w-4xl'>
-                {children}
-              </div>
+              <div className='w-full max-w-4xl'>{children}</div>
             </section>
-
+            {/* @ts-ignore */}
             <RightSidebar />
           </main>
 
           <Bottombar />
-
         </body>
       </html>
     </ClerkProvider>
-
-  )
+  );
 }
